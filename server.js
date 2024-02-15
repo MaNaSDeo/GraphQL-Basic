a9458b27f16962dc157b21b7d9c4aafb1dc7719c
MaNaSDeo <rmnslogin@gmail.com>
2024-02-15 11:27:17 +0530

    GraphQL Schema

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const expressGraphQL = require("express-graphql").graphqlHTTP;
 
 const app = express();
 
+/**Middlewares are tiny functions meant to intercept or modify requests as they come through an express server.*/
 app.use(
   "/graphql",
   expressGraphQL({
